6aecf61927baa41879bc4b8e014252be48f4e938
Jacob Larsson <jlarsson_nr96@hotmail.com>
2017-09-05 11:03:41 +0200

    adding typing for using module import with highchart solid-gauge lib

---
+++ types/highcharts/modules/solid-gauge.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for Highcharts 5.0.14 (solid-gauge module)
+// Project: http://www.highcharts.com/
+// Definitions by: Jacob <https://github.com/Jake91>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import { Static } from "highcharts";
+
+declare function HighchartsSolidGauge(H: Static): Static;
+export = HighchartsSolidGauge;
+export as namespace HighchartsSolidGauge;
